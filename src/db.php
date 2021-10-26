90e51932206ffae416eb3faa98262cafb8531e66
Lamhot Simamora <lamhotsimamora36@gmail.com>
2021-10-26 09:56:22 +0700

    Update db.php

--- src/db.php
+++ src/db.php
@@ -5,12 +5,10 @@
 * Made With Love by Lamhot Simamora
 * https://github.com/lamhotsimamora
 * Free license & open source
-* September@2021
+* Every query has sql injection protection ! 
+* Keep Safe ! And Enjoy !
+* October@2021
 */
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST , GET, OPTIONS");
 
 class DB
 {
@@ -21,38 +19,41 @@ class DB
 	private $database   = null;
 
 	private $conn       = null;
-	
+
 	public  $result     = null;
 	private $query      = null;
 	private $query_where = false;
 	private $query_limit = false;
 	private $query_orderby = false;
-	private $query_where_like =false;
-	private $query_where_not =false;
+	private $query_where_like = false;
+	private $query_where_not = false;
 
 	private $columns    = array();
 	private $wheres     = null;
-	private $where_like =null;
-	private $where_not =null;
+	private $where_like = null;
+	private $where_not = null;
 	private $values     = null;
 	private $limit_number = false;
 	private $order_by_columns = false;
-	private $conn_id=null;
+	private $conn_id = null;
 	private $lock_table = false;
 
 	private $data = null;
 	public $count = null;
 
 	private $all_tables = null;
-	
+
+	public $operator = 'AND';
+
 	private $init = [
+		'server'   => 'YOUR_SERVER',
 		'username' => 'YOUR_USERNAME',
 		'password' => 'YOUR_PASSWORD',
-		'server'   => 'YOUR_SERVER',
 		'database' => 'YOUR_DATABASE'
 	];
 
-	public function __construct(){
+	public function __construct()
+	{
 		$this->username = $this->init['username'];
 		$this->password = $this->init['password'];
 		$this->server   = $this->init['server'];
@@ -61,21 +62,23 @@ class DB
 		$this->connect();
 		$this->error();
 		return $this;
-	}    
+	}
 
-	public function lockTable($permission=null){
-		$permission_access='WRITE';
-		if ($permission){
-			if ($permission['read'] && $permission['read']==true){
+	public function lockTable($permission = null)
+	{
+		$permission_access = 'WRITE';
+		if ($permission) {
+			if ($permission['read'] && $permission['read'] == true) {
 				$permission_access .= ',READ';
 			}
 		}
-		$final_query = 'LOCK TABLES '.$this->table.' '.$permission_access;
+		$final_query = 'LOCK TABLES ' . $this->table . ' ' . $permission_access;
 		$this->query = ($final_query);
 		return $this->proccess();
 	}
 
-	public function unlockTable(){
+	public function unlockTable()
+	{
 		$this->query = ('UNLOCK TABLES');
 		$this->proccess();
 		return $this->toJson();
@@ -84,27 +87,32 @@ class DB
 	/*
 	* Connection Id
 	*/
-	public function getConId(){
+	public function getConId()
+	{
 		$this->query('SELECT CONNECTION_ID()');
 		return $this->conn_id = $this->data['CONNECTION_ID()'];
 	}
 
-	public function getSessionUser(){
+	public function getSessionUser()
+	{
 		$this->query('SELECT SESSION_USER()');
 		return $this->toJson();
 	}
 
-	public function showDatabase(){
+	public function showDatabase()
+	{
 		$this->query('SELECT DATABASE()');
 		return $this->toJson();
 	}
 
-	public function from($val){
+	public function from($val)
+	{
 		$this->table($val);
 		return $this;
 	}
 
-	public function table($val){
+	public function table($val)
+	{
 		$this->table = $val;
 		return $this;
 	}
@@ -117,369 +125,406 @@ class DB
 		return $this;
 	}
 
-	public function limit($val){
+	public function limit($val)
+	{
 		$this->limit_number = $val;
 		$this->queryLimit();
 		return $this;
 	}
 
-	public function orderBy($val){
+	public function orderBy($val)
+	{
 		$this->order_by_columns = $val;
 		$this->queryOrderBy();
 		return $this;
 	}
 
-	private function error(){
-		if ($this->conn->connect_error){
+	private function error()
+	{
+		if ($this->conn->connect_error) {
 			exit("Error when try to connect database !");
 		}
 	}
 
-	private function connect(){
+	private function connect()
+	{
 		try {
 			$conn =  $this->conn = new mysqli(
-								$this->server, 
-								$this->username, 
-								$this->password, 
-								$this->database
-							);
+				$this->server,
+				$this->username,
+				$this->password,
+				$this->database
+			);
 			return $conn;
 		} catch (Exception $e) {
-			exit('[Error] '.$e);
+			exit('[Error] ' . $e);
 		}
 	}
 
-	public function select(){
+	public function select()
+	{
 		$this->columns = func_get_args();
 		return $this;
 	}
 
-	public function whereNot($val){
+	public function whereNot($val)
+	{
 		$this->where_not = $val;
 		$this->queryWhereNot();
 		return $this;
 	}
 
-	public function whereLike($val){
+	public function whereLike($val)
+	{
 		$this->where_like = $val;
 		$this->queryWhereLike();
 		return $this;
 	}
 
-	public function where($val){
+	public function where($val)
+	{
 		$this->wheres = $val;
 		$this->queryWhere();
 		return $this;
 	}
 
-	public function getQuery(){
+	public function getQuery()
+	{
 		return $this->query;
 	}
 
-	public function update($val){
+	public function update($val)
+	{
 		$this->queryUpdate($val);
 		$this->proccess();
 		return $this;
 	}
 
-	public function query($query){
+	public function query($query)
+	{
 		$this->query = $query;
 		$this->proccessWithData();
 		return $this;
 	}
 
-	public function getTables(){
+	public function getTables()
+	{
 		$this->query('select table_name FROM information_schema.tables
-		where table_schema = "'.$this->init['database'].'"');
+		where table_schema = "' . $this->init['database'] . '"');
 		$this->all_tables = $this->proccessWithData();
 		$this->proccessCount();
 		return $this;
 	}
 
-	public function getFields(){
+	public function getFields()
+	{
 		$this->getColumns();
 		return $this;
 	}
 
-	public function getColumns(){
-		$this->query('DESCRIBE '.$this->table);
+	public function getColumns()
+	{
+		$this->query('DESCRIBE ' . $this->table);
 		$this->proccessWithData();
 		$this->proccessCount();
 		return $this;
 	}
 
-	public function getInsertId(){
+	public function getInsertId()
+	{
 		return $this->conn->insert_id;
 	}
 
-	private function queryOrderBy(){
-		if ($this->order_by_columns!=false){
-			$this->query_orderby = ' order by '.$this->order_by_columns;
+	private function removeSingleQuote($str){
+		$str = trim($str);
+		return str_replace("'","",$str);
+	}
+
+	private function queryOrderBy()
+	{
+		if ($this->order_by_columns != false) {
+			$this->query_orderby = ' order by ' . $this->removeSingleQuote($this->order_by_columns);
 		}
 	}
 
-	private function queryUpdate($val){
-		$columns ='';
+	private function queryUpdate($val)
+	{
+		$columns = '';
 		$count_column = count($val);
-		
-		$i=0;
+
+		$i = 0;
 		foreach ($val as $key => $value) {
-			if (is_string($value)){
-				$value='"'.$value.'"';
+			if (is_string($value)) {
+				$value = '"' . $value . '"';
 			}
-			if ($i==0 && ($count_column==1)){
-				$columns .= $key.'='.$value;
-			}else{
-				if ($i==($count_column-1)){
-					$columns .= $key.'='.$value;
-				}else{
-					$columns .= $key.'='.$value.' , ';
+			$key = $this->removeSingleQuote($key);
+			if ($i == 0 && ($count_column == 1)) {
+				$columns .= $key . '=' . $value;
+			} else {
+				if ($i == ($count_column - 1)) {
+					$columns .= $key . '=' . $value;
+				} else {
+					$columns .= $key . '=' . $value . ' , ';
 				}
 			}
-			$i++;	
+			$i++;
 		}
-		$this->query = 'update '.$this->table.' set '.$columns.$this->query_where.'';
+		$this->query = 'update ' . $this->table . ' set ' . $columns . $this->query_where . '';
 		return $this;
 	}
 
-	
+
 	private function queryWhereNot()
 	{
-		if ($this->where_not!=false){
+		if ($this->where_not != false) {
 			$where_not = '';
 			$count_where_not = count($this->where_not);
-			
-			$i=0;
+
+			$i = 0;
 			foreach ($this->where_not as $key => $value) {
-				if (is_string($value)){
+				if (is_string($value)) {
 					$value = $value;
 				}
-				if ($i==0 && ($count_where_not==1)){
-					$where_not = $key. ' <> '.$value.'';
-				}else{
-					if ($i==($count_where_not-1)){
-						$where_not .= $key.' <> '.$value.'';
-					}else{
-						$where_not .= $key.' <> '.$value.' AND ';
+				$key = $this->removeSingleQuote($key);
+				if ($i == 0 && ($count_where_not == 1)) {
+					$where_not = $key . ' <> ' . $value . '';
+				} else {
+					if ($i == ($count_where_not - 1)) {
+						$where_not .= $key . ' <> ' . $value . '';
+					} else {
+						$where_not .= $key . ' <> ' . $value . $this->operator;
 					}
-				}	
+				}
 				$i++;
 			}
-			$this->query_where_not = ' where '.$where_not;
+			$this->query_where_not = ' where ' . $where_not;
 		}
 	}
 
 	private function queryWhereLike()
 	{
-		if ($this->where_like!=false){
+		if ($this->where_like != false) {
 			$where_like = '';
 			$count_where_like = count($this->where_like);
-			
-			$i=0;
+
+			$i = 0;
 			foreach ($this->where_like as $key => $value) {
-				if (is_string($value)){
-					$value = '"%'.$value.'%"';
+				if (is_string($value)) {
+					$value = '"%' . $value . '%"';
 				}
-				if ($i==0 && ($count_where_like==1)){
-					$where_like = $key. ' like '.$value.'';
-				}else{
-					if ($i==($count_where_like-1)){
-						$where_like .= $key.' like '.$value.'';
-					}else{
-						$where_like .= $key.' like '.$value.' AND ';
+				$key = $this->removeSingleQuote($key);
+				if ($i == 0 && ($count_where_like == 1)) {
+					$where_like = $key . ' like ' . $value . '';
+				} else {
+					if ($i == ($count_where_like - 1)) {
+						$where_like .= $key . ' like ' . $value . '';
+					} else {
+						$where_like .= $key . ' like ' . $value . ' '.$this->operator.' ';
 					}
-				}	
+				}
 				$i++;
 			}
-			$this->query_where_like = ' where '.$where_like;
+			$this->query_where_like = ' where ' . $where_like;
 		}
 	}
 
-	private function queryLimit(){
-		if ($this->limit_number !=false){
-			$this->query_limit = ' limit '.$this->limit_number;
+	private function queryLimit()
+	{
+		if ($this->limit_number != false) {
+			$this->query_limit = ' limit ' . $this->removeSingleQuote($this->limit_number);
 		}
 	}
 
-	private function queryGet(){
+	private function queryGet()
+	{
 		$columns = '';
 		$count_column = count($this->columns);
-	
-		foreach ($this->columns as $key => $value) {
-			if ($key==0 && ($count_column==1)){
-				$columns .= $value;
-			}else{
-				if ($key==($count_column-1)){
-					$columns .= $value;
-				}else{
-					$columns .= $value.',';
-				}
-			}	
-		}
-		$this->query =  'select '.$columns.' from '.$this->table.'';
 
-		if ($this->query_where!=false){
+		foreach ($this->columns as $key => $value) {
+			$key = $this->removeSingleQuote($key);
+			if ($key == 0 && ($count_column == 1)) {
+				$columns .= $value;
+			} else {
+				if ($key == ($count_column - 1)) {
+					$columns .= $value;
+				} else {
+					$columns .= $value . ',';
+				}
+			}
+		}
+		$this->query =  'select ' . $columns . ' from ' . $this->table . '';
+
+		if ($this->query_where != false) {
 			$this->queryWhere();
-			$this->query =  $this->query.$this->query_where;
-		}
-		else if ($this->query_where_like != false){
+			$this->query =  $this->query . $this->query_where;
+		} else if ($this->query_where_like != false) {
 			$this->queryWhereLike();
-			$this->query =  $this->query.$this->query_where_like;
-		}
-		else if ($this->query_where_not != false){
+			$this->query =  $this->query . $this->query_where_like;
+		} else if ($this->query_where_not != false) {
 			$this->queryWhereNot();
-			$this->query =  $this->query.$this->query_where_not;
+			$this->query =  $this->query . $this->query_where_not;
 		}
 
 		$this->queryOrderBy();
 
-		if ($this->query_orderby!=false){
-			$this->query =  $this->query.$this->query_orderby;
+		if ($this->query_orderby != false) {
+			$this->query =  $this->query . $this->query_orderby;
 		}
 
 		$this->queryLimit();
 
-		if ($this->query_limit!=false)
-		{	
-			$this->query =  $this->query.$this->query_limit;
+		if ($this->query_limit != false) {
+			$this->query =  $this->query . $this->query_limit;
 		}
 	}
 
-	private function queryDelete(){
-		$this->query ='delete from '.$this->table.$this->query_where;
+	private function queryDelete()
+	{
+		$this->query = 'delete from ' . $this->table . $this->query_where;
 	}
 
-	private function queryInsert(){
-		
+	private function queryInsert()
+	{
+
 		$columns = '';
 		$count_column = count($this->columns);
-	
+
 		foreach ($this->columns as $key => $value) {
-			if ($key==0 && ($count_column==1)){
+			if ($key == 0 && ($count_column == 1)) {
 				$columns .= $value;
-			}else{
-				if ($key==($count_column-1)){
+			} else {
+				if ($key == ($count_column - 1)) {
 					$columns .= $value;
-				}else{
-					$columns .= $value.',';
+				} else {
+					$columns .= $value . ',';
 				}
-			}	
+			}
 		}
 		$values = '';
 		$count_values = count($this->values);
-	
+
 		foreach ($this->values as $key => $value) {
-			if (is_string($value)){
-				$value = '"'.$value.'"';
+			if (is_string($value)) {
+				$value = '"' . $value . '"';
 			}
-			if ($key==0 && ($count_values==1)){
+			$key = $this->removeSingleQuote($key);
+			if ($key == 0 && ($count_values == 1)) {
 				$values .= $value;
-			}else{
-				if ($key==($count_values-1)){
+			} else {
+				if ($key == ($count_values - 1)) {
 					$values .= $value;
-				}else{
-					$values .= $value.',';
+				} else {
+					$values .= $value . ',';
 				}
-			}	
+			}
 		}
 
-		$this->query  = 'insert into '.$this->table.' ( '.$columns.') VALUES ('.$values.')';
+		$this->query  = 'insert into ' . $this->table . ' ( ' . $columns . ') VALUES (' . $values . ')';
 		return $this;
 	}
 
 
-	private function queryWhere(){
-		if ($this->wheres != null ){
+	private function queryWhere()
+	{
+		if ($this->wheres != null) {
 			$wheres = '';
 			$count_where = count($this->wheres);
-			
-			$i=0;
+
+			$i = 0;
 			foreach ($this->wheres as $key => $value) {
-				if (is_string($value)){
-					$value = '"'.$value.'"';
+				if (is_string($value)) {
+					$value = '"' . $value . '"';
 				}
-				if ($i==0 && ($count_where==1)){
-					$wheres .= $key.'='.$value;
-				}else{
-					if ($i==($count_where-1)){
-						$wheres .= $key.'='.$value;
-					}else{
-						$wheres .= $key.'='.$value.' AND ';
+				$key = $this->removeSingleQuote($key);
+				if ($i == 0 && ($count_where == 1)) {
+					$wheres .= $key . '=' . $value;
+				} else {
+					if ($i == ($count_where - 1)) {
+						$wheres .= $key . '=' . $value;
+					} else {
+						$wheres .= $key . '=' . $value . ' '.$this->operator.' ';
 					}
-				}	
+				}
 				$i++;
 			}
-			$this->query_where =' where '.$wheres.'';
+			$this->query_where = ' where ' . $wheres . '';
 		}
 	}
 
-	public function toJson(){
+	public function toJson()
+	{
 		return json_encode($this->data);
 	}
 
-	public function data(){
+	public function data()
+	{
 		return $this->data;
-	}	
+	}
 
-	public function getCount(){
+	public function getCount()
+	{
 		$this->queryGet();
 		$this->proccessCount();
 		return $this;
 	}
 
-	private function proccessCount(){
+	private function proccessCount()
+	{
 		try {
-			$result =$this->conn->query($this->query);
-            if ($result) 
-			{
-                $this->count =  ($result->num_rows) ? $result->num_rows : null;
-            }
+			$result = $this->conn->query($this->query);
+			if ($result) {
+				$this->count =  ($result->num_rows) ? $result->num_rows : null;
+			}
 			return $this->count;
 		} catch (\Throwable $th) {
-			exit('Proccess Count Error Because '.$th);
+			exit('Proccess Count Error Because ' . $th);
 		}
 	}
 
-	public function get(){
+
+	private function proccess()
+	{
+		$this->result = $this->conn->query($this->query);
+		return $this->result ? true : false;
+	}
+
+	private function proccessWithData()
+	{
+		$this->result = $this->conn->query($this->query);
+
+		if ($this->result) {
+			$this->count = $count_data  = $this->result->num_rows;
+			if ($count_data == 1) {
+				$this->data = $this->result->fetch_assoc();
+				return true;
+			} else {
+				$obj = [];
+				$i = 0;
+				while ($row = $this->result->fetch_assoc()) {
+					$obj[$i] = $row;
+					$i++;
+				}
+				$this->data = $obj;
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	
+	public function get()
+	{
 		$this->queryGet();
 		$this->proccessWithData();
 		return $this;
 	}
 
-	public function delete(){
+	public function delete()
+	{
 		$this->queryDelete();
 		$this->proccess();
 		return $this;
 	}
-
-	private function proccess(){
-		$this->result = $this->conn->query($this->query);
-		return $this->result ? true : false;
-	}
-
-	private function proccessWithData(){
-		$this->result = $this->conn->query($this->query);
-
-		if ($this->result){
-			$this->count =$count_data  = $this->result->num_rows;
-			if ($count_data==1){
-				$this->data = $this->result->fetch_assoc();
-				return true;
-			}
-			else
-			{
-				$obj = [];
-	            $i =0 ;
-	            while($row = $this->result->fetch_assoc()) {
-	                $obj[$i] = $row;
-	                $i++;
-	            }
-	            $this->data = $obj;
-	            return true;
-			}
-		}
-		else{
-			return false;
-		}
-	}
 }
+
